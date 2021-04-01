e588c686a44c26c03b1041b6408885a8fa2640dc
GinnyZhao <54597823+GinnyZhao@users.noreply.github.com>
2021-04-01 17:46:10 -0400

    6A done

--- homework_6/main.js
+++ homework_6/main.js
@@ -1,3 +1,6 @@
+// localStorage.clear();
+
+// var carts = [];
 
 
 let getIndex = num => {
@@ -8,6 +11,9 @@ let getIndex = num => {
 
 
 let initDetail = () => {
+    localStorage.setItem("quant", 1);
+    localStorage.setItem("glaze", "No glazing");
+
     var i = localStorage.getItem("i");
     let sub = document.getElementById("sub");
     const text = document.createTextNode(rolls[i].sub);
@@ -27,5 +33,57 @@ let initDetail = () => {
 
     price.appendChild(p);
 
+    
+    q = localStorage.getItem("total") || 0;
+    let quantity = document.getElementById("quantity");
+    quantity.innerText = q;
+    
 }
+
+
+
+let getGlazing = e => {
+    localStorage.setItem("glaze",e);
+    console.log(e);
+}
+
+let getNumber = e => {
+    console.log(e);
+    localStorage.setItem("quant", e);
+}
+
+
+let addToCart = () => {
+    var i = localStorage.getItem("i")
+    var quant = localStorage.getItem("quant");
+    var glaze = localStorage.getItem("glaze");
+
+
+    var cart = JSON.parse(localStorage.getItem("carts"));
+
+    if (cart === null) {
+        cart = []
+    }
+
+    cart.push([i, quant, glaze])
+
+    localStorage.setItem("carts", JSON.stringify(cart));
+    console.log(localStorage.getItem("carts"));
+
+    var total = parseInt(localStorage.getItem("total") || 0);
+    total += parseInt(quant);
+    localStorage.setItem("total", total);
+
+    q = localStorage.getItem("total") || 0;
+    let quantity = document.getElementById("quantity");
+    quantity.innerText = q;
+
+}
+
+let getQuantity = () => {
+    q = localStorage.getItem("total") || 0;
+    let quantity = document.getElementById("quantity");
+    quantity.innerText = q;
+}
+
 
