df422526265c7f85ee84e961f9a2f85a3ea47993
GinnyZhao <54597823+GinnyZhao@users.noreply.github.com>
2021-04-01 23:12:20 -0400

    code done

--- homework_6/main.js
+++ homework_6/main.js
@@ -1,8 +1,4 @@
-// localStorage.clear();
-
-// var carts = [];
-
-
+// get the index of added cinnamon roll
 let getIndex = num => {
     localStorage.setItem("i",num);
     window.location.href='details.html';
@@ -11,6 +7,7 @@ let getIndex = num => {
 
 
 let initDetail = () => {
+    // dynamically change picture and description
     localStorage.setItem("quant", 1);
     localStorage.setItem("glaze", "No glazing");
 
@@ -33,7 +30,8 @@ let initDetail = () => {
 
     price.appendChild(p);
 
-    
+
+    // generate number of items in cart 
     q = localStorage.getItem("total") || 0;
     let quantity = document.getElementById("quantity");
     quantity.innerText = q;
@@ -41,18 +39,19 @@ let initDetail = () => {
 }
 
 
-
+// get which glazing option is selected
 let getGlazing = e => {
     localStorage.setItem("glaze",e);
-    console.log(e);
+
 }
 
+// get the quantity of selected item
 let getNumber = e => {
-    console.log(e);
+
     localStorage.setItem("quant", e);
 }
 
-
+// get information about added item (index, quantity, glazing)
 let addToCart = () => {
     var i = localStorage.getItem("i")
     var quant = localStorage.getItem("quant");
@@ -68,7 +67,6 @@ let addToCart = () => {
     cart.push([i, quant, glaze])
 
     localStorage.setItem("carts", JSON.stringify(cart));
-    console.log(localStorage.getItem("carts"));
 
     var total = parseInt(localStorage.getItem("total") || 0);
     total += parseInt(quant);
@@ -78,8 +76,15 @@ let addToCart = () => {
     let quantity = document.getElementById("quantity");
     quantity.innerText = q;
 
+
+
+    var money = parseFloat(localStorage.getItem("money") || 0)
+    money += parseInt(quant)*parseFloat(rolls[i].money)
+    localStorage.setItem("money", money)
+
 }
 
+// generate number of items in cart in all other pages
 let getQuantity = () => {
     q = localStorage.getItem("total") || 0;
     let quantity = document.getElementById("quantity");
@@ -87,3 +92,100 @@ let getQuantity = () => {
 }
 
 
+let drawcart = () => {
+    let cartList = JSON.parse(localStorage.getItem("carts"));
+
+
+// get subtotal and total 
+    q = localStorage.getItem("total") || 0;
+    let quantity = document.getElementById("quantity");
+    quantity.innerText = q;
+
+    money = localStorage.getItem("money") || 0;
+    let subtotal = document.getElementById("subtotal");
+    subtotal.innerText = "$ " + money;
+    
+    let sum = document.getElementById("sum");
+    sum.innerText = "$ " + (parseFloat(money)+5);
+
+    if (cartList.length !== 0) {
+        var empty = document.getElementById("empty")
+        empty.style["display"] = "none";
+    }
+
+    // generate list of added items
+    for (let i = 0;i < cartList.length;i++) {
+        item = cartList[i];
+        index = item[0];
+        itemName = rolls[index].name + " Cinnamon Roll";
+        img_URL = rolls[index].url;
+        itemPrice = rolls[index].price;
+        itemQuantity = "Quantity: " + item[1];
+        itemGlaze = "Glazing: " + item[2];
+
+        let listing = document.getElementById("checkout-items");
+
+        var newDiv = document.createElement("div");
+        newDiv.setAttribute("class", "newDiv")
+        newDiv.setAttribute("id", index);
+
+
+        newDiv.innerHTML = "<h4>" +itemName+ "</h4>" + "<div class='contentDiv'> <img class='left_pic' src='"+img_URL+"'/> <div class='cart_right'> <p>"+itemPrice+"<p>"+itemQuantity+"</p> <p>"+itemGlaze+"<br><div class='edit'> <p class='modify'> <b> Edit </b> </p> <p class='modify' style='margin-left: 30px' onclick='removeItem("+index+")'> <b> Remove </b> </p>  </div> </p></p></div> </div>"
+
+        listing.appendChild(newDiv);
+
+    }
+
+
+}
+
+// remove items, adjust total amount and the number beside cart
+let removeItem = index => {
+    let cartList = JSON.parse(localStorage.getItem("carts"));
+    let total = localStorage.getItem("total");
+
+    let money = localStorage.getItem("money");
+
+    for (let j=0; j < cartList.length; j++) {
+        if (parseInt(cartList[j][0]) === index) {
+            reduce = cartList[j][1];
+            moneyLose = parseInt(reduce)*parseFloat(rolls[cartList[j][0]].money);
+            cartList.splice(j, 1);
+        }
+    }
+
+    new_total = parseInt(total)- parseInt(reduce);
+    new_money = parseFloat(money)- parseFloat(moneyLose);
+
+    localStorage.setItem("total", new_total);
+    localStorage.setItem("money", new_money);
+
+
+    q = localStorage.getItem("total") || 0;
+    let quantity = document.getElementById("quantity");
+    quantity.innerText = q;
+
+
+    money = localStorage.getItem("money") || 0;
+    let subtotal = document.getElementById("subtotal");
+    subtotal.innerText = "$ " + money;
+    
+    let sum = document.getElementById("sum");
+    sum.innerText = "$ " + (parseFloat(money)+5);
+
+
+
+    localStorage.setItem("carts", JSON.stringify(cartList));
+    
+    var empty = document.getElementById("empty")
+    if (cartList.length === 0) {
+        empty.style["display"] = "block";
+    }
+
+
+    let cancel = document.getElementById(index);
+    cancel.style["display"] = "none";
+
+
+
+}
--- homework_6/rolls.js
+++ homework_6/rolls.js
@@ -4,6 +4,7 @@ var rolls = [
         "sub": "Original Flavored",
         "description": "Our original flavored cinnamon rolls are the all time favorites of our customers. The classic flavor will never let you down.",
         "price": "$3.50 each",
+        "money" : "3.50",
         "url": "img/original.png"},
 
     {
@@ -11,6 +12,7 @@ var rolls = [
         "sub": "Original(Gluten-Free) Flavored",
         "description": "Our original(Gluten-Free) flavored cinnamon rolls preserve the classic flavor of our cinnamon rolls, but also make sure allergies are out of the way!",
         "price": "$3.50 each",
+        "money" : "3.50",
         "url": "img/gluten free.png"
     },
 
@@ -18,7 +20,8 @@ var rolls = [
         "name": "Blackberry",
         "sub": "Blackberry Flavored",
         "description": "Craving for some fruity flavor? Our blackberry flavored cinnamon rolls will be the best choice for you.",
-        "price": "$4.00 each",
+        "price": "$4.00 each",       
+        "money" : "4.00",
         "url": "img/blackberry.png"
     },
     
@@ -27,6 +30,7 @@ var rolls = [
         "sub": "Pumpkin Spice Flavored",
         "description": "What else can beat a pumpkin spice flavored cinnamon roll with a cup of coffee in the morning? Order now to enjoy this autumn vibe!",
         "price": "$4.50 each",
+        "money" : "4.50",
         "url": "img/pumpkin.png"
         },
     
@@ -35,6 +39,7 @@ var rolls = [
         "sub": "Walnut Flavored",
         "description": "Our signature nutty flavored cinnamon rolls are full of nuts! Enjoy this unique texture~",
         "price": "$4.50 each",
+        "money" : "4.50",
         "url": "img/walnut.png"
         },
 
@@ -43,6 +48,7 @@ var rolls = [
         "sub": "Caramel Pecan Flavored",
         "description": "Our signature CP cinnamon rolls are moist, doughy and topped with caramel icing and a sprinkle of chopped pecans. Best choice for sweet lovers!",
         "price": "$4.50 each",
+        "money" : "4.50",
         "url": "img/caramel.png"
         },
 
